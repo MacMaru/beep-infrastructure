2c1f2245b3fd4938ff5653dc3f70acbbb02a90ce
George van Vliet <george@georgevanvliet.nl>
2019-09-05 14:21:21 +0200

    Init CDK

---
+++ bin/beep-infrastructure.ts
@@ -0,0 +1,9 @@
+#!/usr/bin/env node
+import 'source-map-support/register';
+import cdk = require('@aws-cdk/core');
+import { BeepStack } from '../lib/beep-stack';
+
+const app = new cdk.App();
+
+const envEu  = { account: '038855593698', region: 'eu-west-1'};
+new BeepStack(app, 'BeepStack', { env: envEu});
---
+++ lib/beep-stack.ts
@@ -0,0 +1,9 @@
+import cdk = require('@aws-cdk/core');
+
+export class BeepStack extends cdk.Stack {
+  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+
+  }
+}
