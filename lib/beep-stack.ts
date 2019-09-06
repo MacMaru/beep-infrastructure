2276f189bd6affcf8fe454cc678f7791f640611f
George van Vliet <george@georgevanvliet.nl>
2019-09-06 14:03:57 +0200

    Add ECR repositories

--- lib/beep-stack.ts
+++ lib/beep-stack.ts
@@ -1,11 +1,13 @@
 import cdk = require('@aws-cdk/core');
-import {
-  SubnetType,
-  Vpc
-} from "@aws-cdk/aws-ec2";
+import {SubnetType, Vpc} from "@aws-cdk/aws-ec2";
 import {EgressAcl} from "./egress-acl";
 import {IngressAcl} from "./ingress-acl";
 import {ApplicationAcl} from "./application-acl";
+import {
+  Cluster,
+} from "@aws-cdk/aws-ecs";
+import {Repository} from "@aws-cdk/aws-ecr";
+const uuid = require('uuid/v4');
 
 export class BeepStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -68,5 +70,71 @@ export class BeepStack extends cdk.Stack {
         subnetName: 'Application'
       })
     });
+
+    const cluster = new Cluster(this, 'EcsCluster', {
+      vpc: vpc,
+      clusterName: 'BeepProduction',
+    });
+
+    const phpDev = new Repository(this, 'PhpDevRepository', {
+      repositoryName: 'beep-php-dev',
+      lifecycleRules: [
+        {
+          description: 'Retain only the last 10 images',
+          maxImageCount: 10
+        }
+      ],
+    });
+
+    const phpProd = new Repository(this, 'PhpProdRepository', {
+      repositoryName: 'beep-php-prod',
+      lifecycleRules: [
+        {
+          description: 'Retain only the last 10 images',
+          maxImageCount: 10
+        }
+      ],
+    });
+
+    const nginxDev = new Repository(this, 'NginxDevRepository', {
+      repositoryName: 'beep-nginx-dev',
+      lifecycleRules: [
+        {
+          description: 'Retain only the last 10 images',
+          maxImageCount: 10
+        }
+      ],
+    });
+
+    const nginxProd = new Repository(this, 'NginxProdRepository', {
+      repositoryName: 'beep-nginx-prod',
+      lifecycleRules: [
+        {
+          description: 'Retain only the last 10 images',
+          maxImageCount: 10
+        }
+      ],
+    });
+
+    const apiDev = new Repository(this, 'ApiDevRepository', {
+      repositoryName: 'beep-api-dev',
+      lifecycleRules: [
+        {
+          description: 'Retain only the last 10 images',
+          maxImageCount: 10
+        }
+      ],
+    });
+
+    const apiProd = new Repository(this, 'ApiProdRepository', {
+      repositoryName: 'beep-api-prod',
+      lifecycleRules: [
+        {
+          description: 'Retain only the last 10 images',
+          maxImageCount: 10
+        }
+      ],
+    });
+
   }
 }
